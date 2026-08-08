f82d4b5c441bf74a3bccf70e38f6c0c5161adfc1
Saran Kumar Kurapati <sarankumarkurapati@gmail.com>
2026-08-08 17:24:18 +0530

    completed building simple ai study assistant

---
+++ app.py
@@ -0,0 +1,45 @@
+from google import genai
+import os
+from google.genai import types
+import gradio as gr
+
+client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+
+personalities = {
+  "Friendly": """You are a friendly, enthusiastic, and highly encouraging Study Assistant.
+   Your goal is to break down complex concepts into simple, beginner-friendly explanations.
+    Use analogies and real-world examples that beginners can relate to.
+     Always ask a follow-up question to check understanding.""",
+  "Academic": """You are a strictly academic, highly detailed, and professional university Professor.
+  Use precise, formal terminology, cite key concepts and structure your response.
+  Your goal is to break down complex concepts into simple, beginner-friendly explanations.
+  Use analogies and real-world examples that beginners can relate to.
+   Always ask a follow-up question to check understanding."""
+}
+
+def study_assistant(question,persona):
+  system_prompt=personalities[persona]
+  response = client.models.generate_content(
+      model="gemini-3-flash-preview",
+      config= types.GenerateContentConfig(
+          system_instruction=system_prompt,
+          temperature=0.4,
+          max_output_tokens=1000,
+          ),
+      contents=question
+  )
+  return response.text
+
+  demo=gr.Interface(
+    fn=study_assistant,
+    inputs=[gr.Textbox(label="Question",lines=4,placeholder="Ask a Question.."),
+            gr.Dropdown(choices=list(personalities.keys()),value="Friendly",label="Personality") ],
+    outputs=gr.Textbox(label="Explanation",lines=10),
+    title="Study Assistant",
+    description="Ask a question to get simple explanation from AI along with analogies and real world examples"
+)
+
+  demo.launch( server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860)),
+               debug=True)
+
